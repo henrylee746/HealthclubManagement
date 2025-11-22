980baf8a46d96e86ed326650a5b7b8d5db66a9c2
henrylee746 <henry.lee746@gmail.com>
2025-11-21 21:09:12 -0500

    Began work on trainer tab

---
+++ fitness-appc3005/src/app/trainer/columns.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { Checkbox } from "@/components/ui/checkbox";
+
+export type Session = {
+  id: string;
+  date: string;
+  title: string;
+  capacity: number;
+};
+
+export const sessionColumns: ColumnDef<Session>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "date",
+    header: "Date",
+  },
+  {
+    accessorKey: "title",
+    header: "Session",
+  },
+  {
+    accessorKey: "capacity",
+    header: "Space",
+  },
+];
--- fitness-appc3005/src/app/trainer/page.tsx
+++ fitness-appc3005/src/app/trainer/page.tsx
@@ -9,8 +9,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+
 import Header from "../../components/Header";
 import {
   Select,
@@ -21,36 +20,10 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Separator } from "@/components/ui/separator";
-import { IconUserScan } from "@tabler/icons-react";
-import { IconDashboardFilled } from "@tabler/icons-react";
 import { IconCalendarUser } from "@tabler/icons-react";
 import { DataTable } from "../member/data-table";
-import { sessionColumns, Session, Health } from "../member/columns";
+import { sessionColumns, Session } from "./columns";
 import { useState, useEffect } from "react";
-
-async function getData(): Promise<Health[]> {
-  return [
-    {
-      id: "728ed52f",
-      date: new Date().toLocaleDateString("en-CA"),
-      weight: 100,
-      goal: 105,
-      classes: 0,
-      sessions: ["Yoga Class", "Upper Body PT"],
-    },
-  ];
-}
 
 async function getSessions(): Promise<Session[]> {
   return [
@@ -70,12 +43,10 @@ async function getSessions(): Promise<Session[]> {
 }
 
 export default function Member() {
-  const [data, setData] = useState<Health[]>([]);
   const [sessions, setSessions] = useState<Session[]>([]);
-  const [member, setMember] = useState("user1");
+  const [trainer, setTrainer] = useState("trainer1");
 
   useEffect(() => {
-    getData().then(setData);
     getSessions().then(setSessions);
   }, []);
 
@@ -83,147 +54,43 @@ export default function Member() {
     <div className="dark min-h-screen flex flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <Header />
       <h1 className="max-w-s mb-4 text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-        Member's Hub
+        Trainers
       </h1>
-      <div className="flex gap-4">
-        <Select
-          defaultValue="user1"
-          onValueChange={(value) => setMember(value)}
-        >
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="Select a member" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectLabel>Members</SelectLabel>
-              <SelectItem value="user1">Henry Lee</SelectItem>
-              <SelectItem value="user2">John Land</SelectItem>
-              <SelectItem value="user3">Jane Doe</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-        <Dialog>
-          <form>
-            <DialogTrigger asChild>
-              <Button variant="outline">Register New Member</Button>
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
-              <DialogHeader>
-                <DialogTitle>Signup</DialogTitle>
-                <DialogDescription>
-                  Register as a new member here. Click "Save changes" when
-                  you're done.
-                </DialogDescription>
-              </DialogHeader>
-              <div className="flex flex-col gap-4">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="henrylee@example.com"
-                  required
-                />
-                <Label htmlFor="firstName">First Name</Label>
-                <Input id="firstName" type="text" required />
+      <Select
+        defaultValue="trainer1"
+        onValueChange={(value) => setTrainer(value)}
+      >
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Select a member" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Members</SelectLabel>
+            <SelectItem value="trainer1">Coach Chris</SelectItem>
+            <SelectItem value="trainer2">Instructor Mary</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
 
-                <Label htmlFor="lastName">Last Name</Label>
-                <Input id="lastName" type="text" required />
-              </div>
-              <DialogFooter>
-                <DialogClose asChild>
-                  <Button variant="outline">Cancel</Button>
-                </DialogClose>
-                <Button type="submit">Save changes</Button>
-              </DialogFooter>
-            </DialogContent>
-          </form>
-        </Dialog>
-      </div>
-      <div className="flex flex-wrap items-stretch items-center justify-center w-full mt-8 mb-4 gap-8">
-        <Card className="w-full lg:max-w-md md:max-w-sm sm:max-w-sm">
-          <CardHeader>
-            <CardTitle className="flex gap-2 items-center">
-              Profile Management
-              <IconUserScan />
-            </CardTitle>
-            <CardDescription>Update your details/fitness goals</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <form>
-              <div className="flex flex-col gap-4">
-                <CardDescription>Personal Details</CardDescription>
-
-                <Input id="email" type="email" placeholder="Email" />
-                <Input id="firstName" type="text" placeholder="First Name" />
-                <Input id="lastName" type="text" placeholder="Last Name" />
-              </div>
-              <div className="flex flex-col gap-4 mt-6">
-                <CardTitle>Fitness Details</CardTitle>
-                <Label htmlFor="weightTarget"> Weight Target (lbs)</Label>
-                <Input id="weightTarget" type="number" placeholder="e.g. 150" />
-                <Label htmlFor="currWeight">Current Weight</Label>
-                <Input id="currWeight" type="number" placeholder="e.g. 156" />
-              </div>
-            </form>
-          </CardContent>
-          <CardFooter className="flex-col gap-2">
-            <Button type="submit" className="w-full">
-              Update
-            </Button>
-          </CardFooter>
-        </Card>
-        <div className="flex flex-col gap-8">
-          <Card className="w-full md:max-w-md lg:max-w-2xl sm:max-w-sm">
-            <CardHeader>
-              <CardTitle className="flex gap-2 items-center">
-                Dashboard
-                <IconDashboardFilled />
-              </CardTitle>
-              <CardDescription>
-                Check your metrics and upcoming sessions here
-              </CardDescription>
-            </CardHeader>
-            {data.length !== 0 ? (
-              <CardContent>
-                <p className="text-muted-foreground text-sm">
-                  Date: {new Date().toLocaleDateString("en-CA")}
-                </p>
-                <p className="text-sm">Weight: {data[0].weight} lbs</p>
-                <Separator className="my-4" />
-                <div className="flex h-5 items-center space-x-4 text-sm">
-                  <div>Weight Goal: {data[0].goal}</div>
-                  <Separator orientation="vertical" />
-                  <div>Classes Upcoming: {data[0].classes}</div>
-                  <Separator orientation="vertical" />
-                  <div>Upcoming Classes: {data[0].sessions.join(", ")}</div>
-                </div>
-              </CardContent>
-            ) : (
-              ""
-            )}
-            <CardFooter className="flex-col gap-2"></CardFooter>
-          </Card>
-          <Card className="w-full lg:max-w-2xl md:max-w-md sm:max-w-sm">
-            <CardHeader>
-              <CardTitle className="flex gap-2 items-center">
-                Group Class Registration
-                <IconCalendarUser />
-              </CardTitle>
-              <CardDescription>
-                Register for upcoming group sessions here
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <DataTable columns={sessionColumns} data={sessions} />
-            </CardContent>
-            <CardFooter className="flex-col gap-2">
-              <Button type="submit" className="w-full">
-                Register
-              </Button>
-            </CardFooter>
-          </Card>
-        </div>
-      </div>
+      <Card className="w-full lg:max-w-2xl md:max-w-md sm:max-w-sm">
+        <CardHeader>
+          <CardTitle className="flex gap-2 items-center">
+            Group Class Registration
+            <IconCalendarUser />
+          </CardTitle>
+          <CardDescription>
+            Register for upcoming group sessions here
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <DataTable columns={sessionColumns} data={sessions} />
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button type="submit" className="w-full">
+            Register
+          </Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
